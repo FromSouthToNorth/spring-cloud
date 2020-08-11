66d21d60445c6bf5930fb1f49a79475bb5fd10be
FromSouthToNorth <598050554@qq.com>
2020-08-11 19:14:34 +0800

    添加消费模块

--- spring-cloud/cloud-provider-payment8001/src/main/java/com/hy/srpingcloud/controller/PaymentController.java
+++ spring-cloud/cloud-provider-payment8001/src/main/java/com/hy/srpingcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.hy.srpingcloud.entity.CommonResult;
 import com.hy.srpingcloud.entity.Payment;
 import com.hy.srpingcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,9 +16,8 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int save = paymentService.save(payment);
-        log.info("*****插入结果：" + save);
         if (save > 0) {
             return new CommonResult(200, "插入数据成功！", save);
         } else {
@@ -32,7 +28,6 @@ public class PaymentController {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult create(@PathVariable("id") Long id) {
         Payment byIdPayment = paymentService.findByIdPayment(id);
-        log.info("*****插入结果：" + byIdPayment);
         if (byIdPayment != null) {
             return new CommonResult(200, "查询成功！", byIdPayment);
         } else {
